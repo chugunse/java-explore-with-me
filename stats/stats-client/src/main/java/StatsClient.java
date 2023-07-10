2d65c4a60c9ced8afead6f1e808af9170b764751
chugunse <chugunse@yandex.ru>
2023-07-10 16:06:18 +0300

    1

---
+++ stats/stats-client/src/main/java/StatsClient.java
@@ -0,0 +1,40 @@
+import dto.EndpointHitDto;
+import dto.ViewStatsDto;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class StatsClient {
+    private final WebClient client;
+
+    public StatsClient(String serverUrl) {
+        this.client = WebClient.create(serverUrl);
+    }
+
+    public void hitRequest(EndpointHitDto EndpointHitDto) {
+        client.post()
+                .uri("/hit")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .bodyValue(EndpointHitDto)
+                .retrieve()
+                .toBodilessEntity()
+                .block();
+    }
+
+    public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+        return client.get()
+                .uri(uriBuilder -> uriBuilder.path("/stats")
+                        .queryParam("start", start)
+                        .queryParam("end", end)
+                        .queryParam("uris", uris)
+                        .queryParam("unique", unique)
+                        .build())
+                .retrieve()
+                .bodyToFlux(ViewStatsDto.class)
+                .collectList()
+                .block();
+    }
+}
---
+++ stats/stats-server/src/main/java/ru/practicum/ewm/controller/EwmStatsServiceController.java
@@ -0,0 +1,45 @@
+package ru.practicum.ewm.controller;
+
+import dto.EndpointHitDto;
+import dto.ViewStatsDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.ewm.service.EndpointHitService;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@Slf4j
+public class EwmStatsServiceController {
+    private final EndpointHitService service;
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @PostMapping("/hit")
+    @Transactional
+    public EndpointHitDto createEndpointHit(@RequestBody @Valid EndpointHitDto endpointHitDto) {
+        log.info("EwmStatsServiceController - POST: /hit endpointHitDto={}", endpointHitDto);
+        return service.createEndpointHit(endpointHitDto);
+    }
+
+    @GetMapping("/stats")
+    @Transactional
+    public List<ViewStatsDto> getViewStats(
+            @RequestParam String start,
+            @RequestParam String end,
+            @RequestParam(required = false) List<String> uris,
+            @RequestParam(defaultValue = "false") Boolean unique) {
+        log.info("EwmStatsServiceController - GET: /stats start={}, end={}, uris={}, unique={}",
+                start, end, uris, unique);
+        LocalDateTime startLDT = LocalDateTime.parse(start, FORMATTER);
+        LocalDateTime endLDT = LocalDateTime.parse(end, FORMATTER);
+        return service.getViewStats(startLDT, endLDT, uris, unique);
+    }
+}
